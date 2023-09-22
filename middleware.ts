bd5b907d27307fd4848a5b3e6a6b2c5a001a1b42
hyunseung <hyun910625@naver.com>
2023-09-22 14:47:23 +0900

    chore: middleware ignore socket api

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/socket/io"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  // matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
